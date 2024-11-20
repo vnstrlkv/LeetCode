0e5590e50d9564b2d1660cbff6a54d6de6fba901
Ivan Strelkov <vnstrlkv@gmailcom>
2024-11-20 10:17:18 +0700

    125. Valid Palindrome

---
+++ LeetCode.Nunit.Tests/Two Pointers/125. Valid Palindrome.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Problems.Two_Pointers;
+
+namespace LeetCode.Nunit.Tests.TwoPointers
+{
+    public class ValidPalindromeTest_125
+    {
+        [TestCaseSource(nameof(TestCase))]
+        public void Test(string str, bool target)
+        {
+            var result = ValidPalindrome_125.IsPalindrome(str);
+            Assert.That(result, Is.EqualTo(target));
+        }
+
+        public static IEnumerable<object[]> TestCase
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { "A man, a plan, a canal: Panama", true },
+                    new object[] {"cat tac", true },
+                    new object[] {"cat 2tac", false },
+                    new object[] {"", true },
+                };
+            }
+        }
+    }
+}
---
+++ LeetCode/Two Pointers/125. Valid Palindrome.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Two_Pointers
+{
+    public class ValidPalindrome_125
+    {
+        public static bool IsPalindrome(string s)
+        {
+            var resultStr = string.Empty;
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z') && s[i] >= 'A' || (s[i] <= '9') && s[i] >= '0')
+                {
+                    stringBuilder.Append(s[i]);
+                }
+            }
+            var result = stringBuilder.ToString().ToLower();
+            stringBuilder.Clear();
+            stringBuilder.Append(result.Reverse().ToArray());
+            var isPalindrome = result == stringBuilder.ToString();
+            return isPalindrome;
+        }
+    }
+}
